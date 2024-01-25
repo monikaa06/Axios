84425a4c4a0468fd300ff97dfaec71003e35e486
monikaa06 <writetomonika06@gmail.com>
2024-01-25 14:12:44 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import React, {useEffect, useState} from 'react';
+import { Link, useNavigate } from "react-router-dom";
 
 function App() {
+  const[colums, setColums] = useState([])
+  const[records, setRecords] = useState([])
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    axios.get("http://localhost:3030/users")
+    .then(res=> {
+      setColums(Object.keys(res.data[0]))
+      setRecords(res.data)
+    })
+  })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='container mt-5'>
+      <div className="text-end">
+        <Link to="/create" className="btn btn-primary">Add +</Link>
+      </div>
+        <table className='table'>
+          <thead>
+            <tr>
+              {colums.map((c,i) => (
+                <th key={i}>{c}</th>
+              ))}
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+              {
+                records.map((d,i) => (
+                  <tr key={i}>
+                    <td>{d.id}</td>
+                    <td>{d.name}</td>
+                    <td>{d.email}</td>
+                    <td>
+                      <Link to={`/update/${d.id}`} className="btn btn-sm btn-success">Update</Link>
+                      <button onClick={e=> handleSubmit(d.id)}  className="btn btn-sm ms-1 btn-danger">Delete</button>
+                    </td>
+                  </tr>
+                ))
+                }
+          </tbody>
+        </table>
+      </div>
   );
+
+  function handleSubmit(id) {
+    const conf = window.confirm("Do you want to delete")
+    if(conf) {
+      axios.delete("http://localhost:3030/users/"+id)
+      .then(res => {
+        alert("Record has deleted")
+        navigate('/')
+      }).catch(err => console.log(err))
+    }
+  }
 }
 
 export default App;
---
+++ src/AppRouter.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import App from "./App"
+import Add from "./Add"
+import Edit from './Edit'
+
+const AppRouter = () => {
+  return (
+    <div className='AppRouter'>
+    <BrowserRouter>
+    <Routes>
+        <Route path='/' element = {<App/>}/>
+        <Route path='create' element = {<Add/>}/>
+        <Route path='update/:id' element = {<Edit/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
+  )
+}
+
+export default AppRouter
